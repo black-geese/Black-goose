53760a5b73c10b61e540172ca921cb394071d942
xuwoY <13333084654@163.com>
2019-08-08 16:42:05 +0800

    No.01

--- black-goose/src/index.js
+++ black-goose/src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-// import App from './App';
-import Html from './components/home';
+import Html from './App';
+// import Html from './components/home';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<Html />, document.getElementById('root'));
