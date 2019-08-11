f2afae91eddccd718c507c2acfed4f4e7768daec
13831133250 <13831133250@163.com>
2019-08-11 20:36:23 +0800

    头部

---
+++ black-goose/src/html-Xs/zuce/index.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Zuce extends Component {
+    render() {
+        return (
+            <div>
+                789
+            </div>
+        )
+    }
+}
--- black-goose/src/index.js
+++ black-goose/src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import Html from './App';
+// import './index.css';
+// import Html from './App';
 // import Html from './components/home';
+import Html from './html-Xs/index'
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<Html />, document.getElementById('root'));
