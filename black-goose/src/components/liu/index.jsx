511a8af36fe31dc72b3c5786652876c04677951c
lxs1008611 <3414739661@qq.com>
2019-08-11 19:11:57 +0800

    第一次提交

--- black-goose/src/components/liu/index.jsx
+++ black-goose/src/components/liu/index.jsx
@@ -1,11 +1,20 @@
 import React, { Component } from 'react'
 
+//引人组件
+import Nav from './liu-nav'
+
+//引入路由
+import { BrowserRouter } from 'react-router-dom'
+
 export default class Liu extends Component {
     render() {
         return (
-            <div>
-                liu
-            </div>
+            <BrowserRouter>
+                
+                {/* 首页 */}
+                <Nav/>
+                
+            </BrowserRouter>
         )
     }
 }
--- black-goose/src/index.js
+++ black-goose/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import Html from './App';
+// import './index.css';
+import Html from '../src/components/home';
 // import Html from './components/home';
 import * as serviceWorker from './serviceWorker';
 
